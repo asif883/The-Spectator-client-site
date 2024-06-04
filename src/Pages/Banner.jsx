36cffe29bf9c8452c1428b5939e264c746f0a5de
asif883 <asifiqbal95863@gmail.com>
2024-06-04 12:01:43 +0600

    banner & Navbar

---
+++ src/Pages/Banner.jsx
@@ -0,0 +1,57 @@
+
+
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+
+
+const animation = { duration: 30000, easing: (t) => t }
+
+const Banner = () => {
+  
+  const [sliderRef] = useKeenSlider({
+    loop: true,
+    renderMode: "performance",
+    drag: false,
+    created(s) {
+      s.moveToIdx(5, true, animation)
+    },
+    updated(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation)
+    },
+    animationEnded(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation)
+    },
+  })
+  return (
+    <div ref={sliderRef} className="keen-slider h-[450px]">
+      <div className="keen-slider__slide number-slide1">
+          <h1 className="text-xl font-bold mb-2">-- No, you can not buy Berkshire stock at a 99% discount — the stock exchange glitched</h1>
+        <img className="w-full h-fit" src="https://i.ibb.co/qgYSbXM/658c0dd4ec62ab5daf7eda41.webp" alt="" />
+      </div>
+      {/* 2nd slider */}
+      <div className="keen-slider__slide number-slide2">
+       <h1 className="text-xl font-bold mb-2">-- Microsoft is laying off hundreds in its Azure cloud business, sources say</h1>
+        <img className="w-full h-fit" src="https://i.ibb.co/dKPc2R3/NKRZSNOPANMWXLLGRM3-CV52-T2-U.jpg" alt="" />
+      </div>
+      {/* 3rd slider */}
+      <div className="keen-slider__slide number-slide3">
+          <h1 className="text-xl font-bold mb-2">--Gen Z is hot new status symbol: an AmEx card</h1>
+          <img className="w-full h-fit" src="https://i.ibb.co/Czgg5Nw/665a11781cd3b17790422e34.webp" alt="" />
+      </div>
+      {/* 4th slider */}
+      <div className="keen-slider__slide number-slide4">
+          <h1 className="text-xl font-bold mb-2">-- 6 of the most beautiful places in the world, according to someone who is been to 107 countries</h1>
+          <img className="w-full h-fit" src="https://i.ibb.co/Q9ymSMC/6650af2da961b37edf39b243.webp" alt="" />
+      </div>
+      
+      {/* <div className="keen-slider__slide number-slide5">
+
+      </div>
+     
+      <div className="keen-slider__slide number-slide6">
+
+      </div> */}
+    </div>
+  )
+};
+export default Banner;
--- src/Pages/Header/Navbar.jsx
+++ src/Pages/Header/Navbar.jsx
@@ -1,46 +1,44 @@
-
+import { Link, NavLink } from "react-router-dom";
+import { IoSearchSharp } from "react-icons/io5";
 
 const Navbar = () => {
     return (
-    <div className="navbar bg-base-100">
-        <div className="navbar-start">
-            <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Item 1</a></li>
-                <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-                </li>
-                <li><a>Item 3</a></li>
-            </ul>
-            </div>
-            <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li>
-                <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-                </details>
-            </li>
-            <li><a>Item 3</a></li>
-            </ul>
-        </div>
-        <div className="navbar-end">
-            <a className="btn">Button</a>
-        </div>
-    </div>
+          <div className="my-6 max-w-7xl mx-auto">
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="mr-5 text-2xl font-medium hover:text-[#3A8CFB]">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-44">
+                        <li><Link className="text-lg font-medium">About</Link></li>
+                        <li><Link className="text-lg font-medium">Contact Us</Link></li>  
+                    </ul>
+                  </div>
+                  <IoSearchSharp className="text-2xl font-medium hover:text-[#3A8CFB]"></IoSearchSharp>
+                </div>
+                <div className="navbar-center">
+                    <div className="">
+                         <h1 className="text-5xl mb-4 font-bold">The Daily Updates</h1>  
+                     </div>
+                </div>
+                <div className="navbar-end">
+                     <div className="">
+                        <NavLink to='/subscription' className='mr-3 font-medium'>Subscription</NavLink>
+                        <button className="bg-[#3A8CFB] text-white px-4 py-2 rounded-xl text-lg font-medium">Login</button>
+                    </div>
+                 </div>
+           </div>
+                
+              <nav className="flex justify-center border-y py-3">
+               <NavLink to='/' className='mr-3 font-medium'>Home</NavLink>            
+                <NavLink to='/add-articles' className='mr-3 font-medium'>Add Articles</NavLink>            
+                <NavLink to='/all-articles' className='mr-3 font-medium'>All Articles</NavLink>                        
+                <NavLink to='/dashboard' className='mr-3 font-medium'>Dashboard</NavLink>            
+                <NavLink to='/my-articles' className='mr-3 font-medium'>My Articles</NavLink>            
+                <NavLink to='/premium-articles' className='mr-3 font-medium'>Premium Articles</NavLink> 
+              </nav>
+          </div>
     );
 };
 
