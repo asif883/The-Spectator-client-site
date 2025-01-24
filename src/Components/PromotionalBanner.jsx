90cbbb2b313983160ba22e15468b79c0603f2a84
asif883 <asifiqbal95863@gmail.com>
2025-01-24 20:03:01 +0600

    responsive promotional banner

--- src/Components/PromotionalBanner.jsx
+++ src/Components/PromotionalBanner.jsx
@@ -1,20 +1,22 @@
 import { Link } from 'react-router-dom';
-import banner from '../assets/Friday_Special_Offer_Banner_1080x400.png'
 import '../Css/font.css'
 
 const PromotionalBanner = () => {
-    // style={{backgroundImage : `url(${banner})`, backgroundSize: 'cover'}} className='h-[400px] object-cover relative'
     return (
-        <div >
+        <div className=''>
              <div className='text-[#C61947] text-center '>
-                <img src={banner} alt="" className='w-full' />
-                  <h3 className="text-6xl uppercase text-font font-semibold mb-3">Friday Special</h3>
-                  <h1 className="text-7xl text-font  font-semibold mb-4">
-                     20 % OFF
-                  </h1>
-                  <Link to="/subscription" className='bg-[#C61947] text-white p-3 uppercase text-font text-xl '>
-                       Get the offer
-                  </Link>
+                <div className='relative'>
+                     <img src='https://cdn.gyftr.com/blog/wp-content/uploads/2024/11/14081129/BlackFriday-1.png' alt="" className='w-full' />
+                     <div className='absolute right-3 md:right-10 lg:right-20 top-3 md:top-10 lg:top-20'>
+                         <h1 className="text-[#EC2025] text-xl md:text-7xl text-font  font-semibold mb-1 md:mb-4">
+                            UP TO <br /> 40% OFF
+                         </h1>
+                         <Link to="/subscription" className='bg-[#EC2025] text-white p-1 md:p-3 uppercase text-font text-sm md:text-xl '>
+                              Get the offer
+                         </Link>
+                    </div>
+               </div>
+                
              </div>
         </div>
     );
